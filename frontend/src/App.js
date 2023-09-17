640e6efc3f1a0e49a02faff487b452441bd7800d
Yash Khare <yashkhare189@gmail.com>
2023-09-17 07:50:55 +0530

    Error Page addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,27 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Problem from "./components/Problems/problemList";
+import Error from "./components/Error/error"
 
 function App() {
-	const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
 
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      {user ? (
+        <>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/add-problem" exact element={<Problem />} />
+        </>
+      ) : (
+        <Route path="/" element={<Navigate replace to="/login" />} />
+      )}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="*" element={<Error />} /> //Wrong route
+    </Routes>
+  );
 }
 
 export default App;
