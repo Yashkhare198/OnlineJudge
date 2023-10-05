e30d91facab9c752b7042df4bba18cfcb65de862
Yash Khare <yashkhare189@gmail.com>
2023-10-05 18:47:57 +0530

    Modified About Page

--- frontend/src/components/About/about.js
+++ frontend/src/components/About/about.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const about = () => {
+const About = () => {
   return (
-    <div>Hi, Welcome to CodeHub ! </div>
-  )
-}
+    <div className="flex justify-center items-center h-screen bg-blue-900">
+      <div className="text-center p-8 bg-white rounded-lg shadow-lg text-black">
+        <h1 className="text-5xl font-semibold mb-4">Welcome to CodeHub!</h1>
+        <p className="text-2xl text-gray-700 mb-6">
+          Your one-stop destination for problem-solving and coding inspiration.
+        </p>
+        <p className="text-xl text-gray-600">
+          Made with ❤️ in India.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default about
+export default About;
